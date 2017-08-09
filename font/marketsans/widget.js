9ad42d2a15db83dabc0c1c58145622b39dd5c25f
Ramu,Raja(rar) <rar@ebay.com>
2017-08-09 15:01:37 -0700

    Update widget.js

--- font/marketsans/widget.js
+++ font/marketsans/widget.js
@@ -1,6 +1,31 @@
 'use strict';
 var lassoLoader = require('lasso-loader').async;
 
+/* Font Face loader */
+(function () {
+    function fontLoader () {
+        var fontFaceSet = document.fonts;
+        if (fontFaceSet && fontFaceSet.load) {
+            fontFaceSet.load('1em Market Sans').then(function() {
+                updateLocalStorage();
+            });
+        } else if (FontFaceObserver) {
+            var marketsans = new FontFaceObserver('Market Sans');
+            if (marketsans && marketsans.load) {
+                marketsans.load().then(function () {
+                    updateLocalStorage();
+                });
+            }
+        }
+    }
+    function updateLocalStorage () {
+        if (localStorage && !localStorage.getItem('ebay-font-class')) {
+            localStorage.setItem('ebay-font-class', true);
+        }	
+    }
+    fontLoader();
+})();
+
 function init() {
     window.addEventListener('load', function () {
         if (localStorage && !localStorage.getItem('ebay-font-class')) {
