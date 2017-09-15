3073a678d1ad88cd41c71e1701e13e549f536814
Raja Ramu <rar@ebay.com>
2017-09-14 22:57:45 -0700

    Fixing safari and IE10 issues

--- font/marketsans/fontloader.js
+++ font/marketsans/fontloader.js
@@ -5,24 +5,39 @@ var lassoLoader = require('lasso-loader').async;
 
 var fontFaceSet = document.fonts;
 var FONT_CLASS_NAME = 'font-marketsans';
+var isSafari10 = null;
 
 function updateLocalStorage() {
     if (localStorage) {
         localStorage.setItem('ebay-font', FONT_CLASS_NAME);
     }
 }
+function isSafari10Bug() {
+    if (isSafari10 === null) {
+      if (/Apple/.test(window.navigator.vendor)) {
+        var match = /AppleWebKit\/([0-9]+)(?:\.([0-9]+))(?:\.([0-9]+))/.exec(window.navigator.userAgent);
+        isSafari10 = !!match && parseInt(match[1], 10) < 603;
+      } else {
+        isSafari10 = false;
+      }
+    }
+    return isSafari10;
+};
 
 function loadFont() {
     // check for fontfaceset else load polyfill before invoking fontloader
-    if (fontFaceSet && fontFaceSet.load) {
+    if (fontFaceSet && fontFaceSet.load && !isSafari10Bug()) {
         fontFaceSet.load('1em Market Sans');
         fontFaceSet.load('bold 1em Market Sans');
         fontFaceSet.ready.then(updateLocalStorage);
     } else {
-        lassoLoader('font-async-observer', function() {
+        lassoLoader('font-async-observer', function(err) {
+            if (err) {
+                return;
+            }
             var marketsansRegular = new FontFaceObserver('Market Sans');
             var marketsansBold = new FontFaceObserver('Market Sans', { weight: 'bold' });
-            Promise.all([marketsansRegular.check(), marketsansBold.check()]).then(updateLocalStorage);
+            Promise.all([marketsansRegular.load(), marketsansBold.load()]).then(updateLocalStorage);
         });
     }
 }
